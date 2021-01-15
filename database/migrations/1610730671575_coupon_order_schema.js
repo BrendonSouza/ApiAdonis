10130f864f7b0ffc067e0e7c6a9c426ddd40806b
Brendon Souza <souzab@pop-os.localdomain>
2021-01-15 19:55:41 -0300

    :wolf: criando tabela order_item

--- database/migrations/1610730671575_coupon_order_schema.js
+++ database/migrations/1610730671575_coupon_order_schema.js
@@ -7,6 +7,11 @@ class CouponOrderSchema extends Schema {
   up () {
     this.create('coupon_orders', (table) => {
       table.increments()
+      table.integer('coupon_id').unsigned()
+      table.integer('order_id').unsigned()
+      table.decimal('discount',12,2).defaultTo(0.0)
+      table.foreign('coupon_id').references('id').inTable('coupons').onDelete('cascade')
+      table.foreign('order_id').references('id').inTable('orders').onDelete('cascade')
       table.timestamps()
     })
   }
