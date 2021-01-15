277ab619257b32e6b2b62755ac6cd032696cd093
Brendon Souza <souzab@pop-os.localdomain>
2021-01-15 13:13:22 -0300

    :wolf:criando tabelas de produtos,categoria e img

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,9 +7,13 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 80)
+      table.string('surname',200)
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      //não curti esse relacionamento eu faria o inverso. Colocaria uma fk de user em image e nao pre
+      //cisaria de outra migration só para setar o relacionamento
+      table.integer('image_id').unsigned()
       table.timestamps()
     })
   }
