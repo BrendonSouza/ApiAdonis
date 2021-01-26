7f921ed71f995de4807c94c7e6559bc0ed5b3b1a
BrendonSouza <brendonqueiros@hotmail.com>
2021-01-26 10:41:18 -0300

    :wolf: model OrderItem criado

--- app/Models/OrderItem.js
+++ app/Models/OrderItem.js
@@ -4,8 +4,14 @@
 const Model = use('Model')
 
 class OrderItem extends Model {
+  static get traits() {
+    return [App / Models / Traits / NoTimestamps]
+  }
   product() {
     return this.belongsTo('App/Models/Products')
+  }
+  order() {
+    return this.belongsTo('App/Models/Orders')
   }
 }
 
